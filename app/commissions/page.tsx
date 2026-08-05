d302dc2546938ac5061f381f990df974a07c6d05
Mohammad Rumman <jah.hasant@gmail.com>
2026-08-06 02:17:15 +0600

    T-Shirt section

--- app/commissions/page.tsx
+++ app/commissions/page.tsx
@@ -158,6 +158,51 @@ export default function CommissionsPage() {
           </div>
         </div>
       </section>
+
+      <section className="tshirt-product" aria-labelledby="tshirt-heading">
+        <div className="tote-bag-content tshirt-content">
+          <h2 id="tshirt-heading">T-shirt</h2>
+          <p className="tote-bag-description">
+            Put your unique spin on a wardrobe essential and order your shirt designs - available as an exclusive add-on when you book your commission.
+          </p>
+
+          <div className="tote-bag-swatches" aria-label="Available colours">
+            {[1, 2, 3, 4, 5].map((swatch) => (
+              <Image key={swatch} src={`/phone-case-swatch-${swatch}.svg`} alt={`Colour option ${swatch}`} width={24} height={24} />
+            ))}
+          </div>
+
+          <div className="tote-bag-details">
+            <details open>
+              <summary>PRODUCT DETAILS</summary>
+              <p>
+                Quantity depends on your chosen canvas size, but typically 20 to 50 pieces allow for a diverse range of colours and textures. During our consultation, we will discuss the &quot;hero&quot; pieces you definitely want included versus the &quot;texture&quot; pieces I can use for background layering.
+              </p>
+            </details>
+            <details>
+              <summary>LOREM IPSUM</summary>
+              <p>Additional product information will be available here.</p>
+            </details>
+            <details>
+              <summary>LOREM IPSUM</summary>
+              <p>Additional product information will be available here.</p>
+            </details>
+          </div>
+        </div>
+
+        <div className="tshirt-gallery">
+          {[1, 2, 3, 4].map((image) => (
+            <Image
+              key={image}
+              src={`/tshirt-product-${image}.png`}
+              alt={`Custom printed T-shirt example ${image}`}
+              width={385}
+              height={306}
+              sizes="(max-width: 800px) 46vw, 28vw"
+            />
+          ))}
+        </div>
+      </section>
     </>
   );
 }
