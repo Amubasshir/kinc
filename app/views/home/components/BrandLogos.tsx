d8c47a358d97d1977566b1330f7c411715a45698
Mohammad Rumman <jah.hasant@gmail.com>
2026-08-10 08:50:05 +0600

    Used actual brand logos

--- app/views/home/components/BrandLogos.tsx
+++ app/views/home/components/BrandLogos.tsx
@@ -3,14 +3,12 @@ import Image from "next/image";
 export default function BrandLogos() {
   return (
     <section className="brand-logos flex h-[168px] items-center justify-center overflow-hidden rounded-[20px] max-[700px]:h-[130px] max-[700px]:rounded-none max-[700px]:bg-transparent" aria-label="Trusted brands">
-      <Image
-        className="block h-auto w-full max-w-[1360px] max-[700px]:w-[350px] max-[700px]:max-w-full"
-        src="/brand-logos.png"
-        alt="Atlassian, hipages, Humanforce and Camilla"
-        width={1360}
-        height={168}
-        sizes="100vw"
-      />
+      <div className="grid w-full max-w-[563px] grid-cols-[198fr_86fr_115fr_68fr] items-center gap-8 max-[700px]:gap-3">
+        <Image className="block h-auto w-full" src="/brand-atlassian.svg" alt="Atlassian" width={198} height={32} />
+        <Image className="block h-auto w-full" src="/brand-hipages.svg" alt="hipages" width={86} height={31} />
+        <Image className="block h-auto w-full" src="/brand-humanforce.svg" alt="Humanforce" width={115} height={22} />
+        <Image className="block h-auto w-full" src="/brand-camilla.svg" alt="Camilla" width={68} height={25} />
+      </div>
     </section>
   );
 }
